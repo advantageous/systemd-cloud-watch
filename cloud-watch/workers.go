2dde5ee604c6ab39176b40a7b350711bb514df75
RichardHightower <richard.m.hightower@gmail.com>
2016-12-02 17:33:55 -0800

    seek start

--- cloud-watch/workers.go
+++ cloud-watch/workers.go
@@ -194,12 +194,13 @@ func RunWorkers(configFilename string, logger *Logger) error {
 	defer journal.Close()
 	defer logger.Info.Println("Leaving main run method")
 
-	//err = journal.SeekTail(); if err != nil {
-	//	logger.Error.Println("Unable to seek to end of systemd journal")
-	//	return err
-	//} else {
-	//	logger.Info.Println("Success: Seek to end of systemd journal")
-	//}
+	err = journal.SeekHead(); if err != nil {
+		logger.Error.Println("Unable to seek to end of systemd journal")
+		return err
+	} else {
+		logger.Info.Println("Success: Seek to end of systemd journal")
+	}
+
 	//
 	//_, err = journal.PreviousSkip(10); if err != nil {
 	//	logger.Error.Println("Unable to rewind 10 after seeking to end of systemd journal")
